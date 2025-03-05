704e09e7ae4a33834c3186db7a262f1615284c14
YJOH11 <yjohmail@naver.com>
2025-03-05 14:14:20 +0900

    [Feat] add accommodation Photo,Room ENTITY & modified(add @OneToMany)

---
+++ mingle/src/main/java/com/example/mingle/accommodation/domain/AccommodationRoom.java
@@ -0,0 +1,32 @@
+package com.example.mingle.accommodation.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tbl_accommodation_room_info")
+public class AccommodationRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "accommodation_room_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "accommodation_id")  // 숙소와 연결된 외래 키
+    private Accommodation accommodation;
+
+    @Column(name = "accommodation_room_cost")
+    private int cost;
+
+    @Column(name = "accommodation_room_min")
+    private int minCapacity;
+
+    @Column(name = "accommodation_room_max")
+    private int maxCapacity;
+
+    @Column(name = "accommodation_room_name")
+    private String name;
+}
