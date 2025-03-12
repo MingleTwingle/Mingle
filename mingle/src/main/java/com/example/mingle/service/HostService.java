abbb0ca3f5eba30d110de52db0f0d7af68b0e7ea
Dain Jeong <dainjeong1004@gmail.com>
2025-03-12 11:34:09 +0900

    [Feat] solve register duplication issue between guest<->host

--- mingle/src/main/java/com/example/mingle/service/HostService.java
+++ mingle/src/main/java/com/example/mingle/service/HostService.java
@@ -1,6 +1,7 @@
 package com.example.mingle.service;
 
 import com.example.mingle.domain.Host;
+import com.example.mingle.repository.GuestRepository;
 import com.example.mingle.repository.HostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,11 +16,13 @@ import java.util.Optional;
 public class HostService {
 
     private final HostRepository hostRepository;
+    private final GuestRepository  guestRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public HostService(HostRepository hostRepository, PasswordEncoder passwordEncoder) {
+    public HostService(HostRepository hostRepository, GuestRepository guestRepository, PasswordEncoder passwordEncoder) {
         this.hostRepository = hostRepository;
+        this.guestRepository = guestRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -31,8 +34,12 @@ public class HostService {
     }
 
     private void validateDuplicationMember(Host host, BindingResult result) {
-        hostRepository.findByName(host.getIdid())
+        hostRepository.findByIdid(host.getIdid())
                 .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+        guestRepository.findByIdid(host.getIdid())
+                .ifPresent(g -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
